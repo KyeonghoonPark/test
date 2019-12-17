eaddc5270daf4f5204b127023e408a3b4a74aafe
KyeonghoonPark <asus4862@naver.com>
2019-12-18 00:41:45 +0900

    #1 commit test

--- MyApplication/app/src/main/java/com/smtm/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/smtm/myapplication/MainActivity.kt
@@ -8,6 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //ddddd
+        //dddddxlskfjkdfk
+        //test2
+        //develop
     }
 }
